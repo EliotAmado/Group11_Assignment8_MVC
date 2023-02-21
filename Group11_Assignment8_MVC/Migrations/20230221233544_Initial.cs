58f68567efe6800e9ff791d8aa0be81327515b07
Mark Briggs <markbriggs321@gmail.com>
2023-02-21 16:47:29 -0700

    Finished Database

---
+++ Group11_Assignment8_MVC/Migrations/20230221233544_Initial.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Group11_Assignment8_MVC.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CategoryName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    TaskId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Task = table.Column<string>(nullable: false),
+                    Due_Date = table.Column<DateTime>(nullable: false),
+                    Quadrant = table.Column<int>(nullable: false),
+                    Completed = table.Column<bool>(nullable: false),
+                    CategoryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.TaskId);
+                    table.ForeignKey(
+                        name: "FK_Tasks_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[] { 1, "Home" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[] { 2, "School" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[] { 3, "Work" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[] { 4, "Church" });
+
+            migrationBuilder.InsertData(
+                table: "Tasks",
+                columns: new[] { "TaskId", "CategoryId", "Completed", "Due_Date", "Quadrant", "Task" },
+                values: new object[] { 2, 1, false, new DateTime(2023, 2, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, "Clean the kitchen" });
+
+            migrationBuilder.InsertData(
+                table: "Tasks",
+                columns: new[] { "TaskId", "CategoryId", "Completed", "Due_Date", "Quadrant", "Task" },
+                values: new object[] { 1, 2, false, new DateTime(2023, 2, 24, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "Mission 8" });
+
+            migrationBuilder.InsertData(
+                table: "Tasks",
+                columns: new[] { "TaskId", "CategoryId", "Completed", "Due_Date", "Quadrant", "Task" },
+                values: new object[] { 4, 3, false, new DateTime(2023, 2, 22, 0, 0, 0, 0, DateTimeKind.Unspecified), 4, "Go to work" });
+
+            migrationBuilder.InsertData(
+                table: "Tasks",
+                columns: new[] { "TaskId", "CategoryId", "Completed", "Due_Date", "Quadrant", "Task" },
+                values: new object[] { 3, 4, false, new DateTime(2023, 2, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), 3, "Ministering" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_CategoryId",
+                table: "Tasks",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
---
+++ Group11_Assignment8_MVC/Models/TaskContext.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Group11_Assignment8_MVC.Models
+{
+    public class TaskContext : DbContext
+    {
+        //Constructor
+        public TaskContext (DbContextOptions<TaskContext> options) : base (options)
+        {
+            //Leave blank
+        }
+
+        public DbSet<AddTaskResponce> Tasks { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Category>().HasData(
+                new Category
+                {
+                    CategoryId = 1,
+                    CategoryName = "Home"
+                },
+                new Category
+                {
+                    CategoryId = 2,
+                    CategoryName = "School"
+                },
+                new Category
+                {
+                    CategoryId = 3,
+                    CategoryName = "Work"
+                },
+                new Category
+                {
+                    CategoryId = 4,
+                    CategoryName = "Church"
+                }
+                );
+
+            mb.Entity<AddTaskResponce>().HasData(
+                new AddTaskResponce
+                {
+                    TaskId = 1,
+                    Task = "Mission 8",
+                    Due_Date = DateTime.ParseExact("24/02/2023", "dd/MM/yyyy", null),
+                    Quadrant = 1,
+                    Completed = false,
+                    CategoryId = 2
+                },
+                new AddTaskResponce
+                {
+                    TaskId = 2,
+                    Task = "Clean the kitchen",
+                    Due_Date = DateTime.ParseExact("25/02/2023", "dd/MM/yyyy", null),
+                    Quadrant = 2,
+                    Completed = false,
+                    CategoryId = 1
+                },
+                new AddTaskResponce
+                {
+                    TaskId = 3,
+                    Task = "Ministering",
+                    Due_Date = DateTime.ParseExact("21/02/2023", "dd/MM/yyyy", null),
+                    Quadrant = 3,
+                    Completed = false,
+                    CategoryId = 4
+                },
+                new AddTaskResponce
+                {
+                    TaskId = 4,
+                    Task = "Go to work",
+                    Due_Date = DateTime.ParseExact("22/02/2023", "dd/MM/yyyy", null),
+                    Quadrant = 4,
+                    Completed = false,
+                    CategoryId = 3
+                }
+                );
+        }
+    }
+}
